c6fdcab5f3ec2962c3d5b5fff93e89cfec5c4c45
kathsup <katherinencarvallo22@gmail.com>
2025-02-07 08:54:36 -0600

    azul

--- src/prueba_4/pruebaChungUn.java
+++ src/prueba_4/pruebaChungUn.java
@@ -12,6 +12,6 @@ public class pruebaChungUn {
     public static void main (String [] args ){
         System.out.println("Commit Chung Un");
     
-    
+        System.out.println("hola");
     }
 }
---
+++ src/prueba_4/pruebakath.java
@@ -0,0 +1,7 @@
+package prueba_4;
+
+public class pruebakath {
+    public static void main(String[] args) {
+        System.out.println("qwerty");
+    }
+}
